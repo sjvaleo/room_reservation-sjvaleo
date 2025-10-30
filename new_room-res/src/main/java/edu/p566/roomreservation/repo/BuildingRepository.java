1061e924450d6feccf8e100cee9eeaec4751a1e9
sjvaleo <sjvaleo@iu.edu>
2025-10-30 05:12:55 +0000

    new git - near complete

--- new_room-res/src/main/java/edu/p566/roomreservation/repo/BuildingRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.p566.roomreservation.repo;
-
-import edu.p566.roomreservation.entity.Building;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuildingRepository extends JpaRepository<Building, Long> { }
